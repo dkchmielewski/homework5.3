74c023912422a86221d1385fefff0344fdaf628e
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-19 21:32:15 -0400

    Deleted YearCheck constructor and made boolean isLeap method public

--- src/YearCheck.java
+++ src/YearCheck.java
@@ -1,10 +1,6 @@
 public class YearCheck {
 
-    YearCheck() {
-
-    }
-
-    boolean isLeap(int year) {
+    public boolean isLeap(int year) {
         boolean firstCondition = year % 4 == 0 && year % 100 != 0;
         boolean secondCondition = year % 400 == 0;
         return firstCondition || secondCondition;
